f2976389812dda634327f0f6faacb8d52adb28d0
Betvic23 <brightndubisi23@gmail.com>
2023-12-04 14:56:46 -0800

    exactly c

---
+++ 0x03-python-data_structures/5-no_c.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def no_c(my_string):
+    new_str = ""
+    for element in my_string:
+        if element != 'c' and element != 'C':
+            new_str += element
+        return new_str    
