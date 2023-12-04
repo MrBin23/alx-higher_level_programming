8dd75d423181dff279f3e6fcd7ec5976218c75c7
Betvic23 <brightndubisi23@gmail.com>
2023-12-04 15:00:19 -0800

    minus c

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -2,6 +2,6 @@
 def no_c(my_string):
     new_str = ""
     for element in my_string:
-        if element != 'c' and element != 'C':
+        if element != "c" and element != "C"
             new_str += element
         return new_str    
